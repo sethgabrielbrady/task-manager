baa583cec8e2c03cf4a20c7658b5bfb9b2352883
Seth Brady <sethgabrielbrady@gmail.com>
2019-07-18 14:48:29 -0400

    updated the css and vue file

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var app = new Vue({
   data: {
     tasks: [
       { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
-      { id: 2, name: 'Todo 2', description: 'This is another todo', completed: false },
+      { id: 2, name: 'Todo 2', description: 'This is another todo', completed: true },
       { id: 3, name: 'Todo 3', description: 'This is yet another todo', completed: false },
-      { id: 4, name: 'Todo 4', description: 'This is also a todo', completed: false }
+      { id: 4, name: 'Todo 4', description: 'This is also a todo', completed: true }
     ],
     message: "Hello World!!!"
+  },
+  computed: {
+    completedTasks: function() {
+      return this.tasks.filter( item => item.completed == true );
+    },
+    todoTasks: function(){
+      return this.tasks.filter ( item => item.completed == false );
+    }
   }
 });
